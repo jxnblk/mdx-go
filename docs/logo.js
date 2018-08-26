4df9aaec8ac68c5876c2c07a46593c0ca4b1a602
Brent Jackson <jxnblk@gmail.com>
2018-08-25 21:18:49 -0400

    Adjust logo

--- docs/logo.js
+++ docs/logo.js
@@ -1,23 +1,7 @@
 import React from 'react'
 
-const M = [
-  'M 0 0',
-  'L 10 5',
-  'L 20 0',
-  'L 20 20',
-  'L 0 20',
-  'z'
-].join(' ')
-
-const X = [
-  'M 0 0',
-  'L 32 32',
-  'M 32 0',
-  'L 0 32'
-].join(' ')
-
-// //
-const F = [
+// forward //
+const forward = [
   'M 16 0',
   'L 48 0',
   'L 32 32',
@@ -26,7 +10,7 @@ const F = [
 ].join(' ')
 
 // \\
-const B = [
+const backward = [
   'M 0 0',
   'L 32 0',
   'L 48 32',
@@ -34,120 +18,52 @@ const B = [
   'z',
 ].join(' ')
 
-export default ({
-  width = 1024,
-  color = 'tomato',
-  bg = '#200'
+export const Logo = ({
+  size = 64,
+  color = '#fff',
+  bg = '#0d0'
 }) =>
   <svg
-    viewBox='0 0 128 64'
-    width={width}
-    height={width / 2}
-    style={{
-      fontFamily: '"Avenir Next", system-ui, sans-serif',
-      fontWeight: 700,
-      fontSize: 12,
-      textTransform: 'uppercase',
-      letterSpacing: '0.5em'
-    }}
-  >
-    <rect
+    viewBox='0 0 160 160'
+    width={size}
+    height={size}>
+    <circle
       fill={bg}
-      width='128'
-      height='64'
+      cx='80'
+      cy='80'
+      r='80'
     />
-    <g fill={color}>
-      <path transform='translate(28 16)' d={B} />
-      <path transform='translate(76 16)' d={B} />
-      <path transform='translate(28 16)' fill='#000' opacity={1/4} d={B} />
-      <path transform='translate(76 16)' fill='#000' opacity={1/4} d={B} />
-      <path transform='translate(4 16)' d={F} />
-      <path transform='translate(52 16)' d={F} />
+    <g fill={color} transform='translate(20 60)'>
+      <path transform='translate(24 0)' d={backward} />
+      <path transform='translate(72 0)' d={backward} />
+      <path transform='translate(24 0)' fill={bg} opacity={1/4} d={backward} />
+      <path transform='translate(72 0)' fill={bg} opacity={1/4} d={backward} />
+      <path transform='translate(0 0)' d={forward} />
+      <path transform='translate(48 0)' d={forward} />
     </g>
   </svg>
 
-const colors = {
-  dark: '#2a2824',
-  paper: '#fea',
-  green: '#0f0',
-  darkgreen: '#0c0',
-  black: '#000'
-}
-
-const circle = (
-<g fill='#030'>
-  <circle
-    cx='64'
-    cy='32'
-    r='16'
-  />
-  <rect
-    fill='#000'
-    width='20'
-    height='20'
-    transform='translate(54 22)'
-  />
-  <path
-    fill='none'
-    stroke='#000'
-    strokeWidth='2'
-    d={X}
-    transform='translate(48 16)'
-  />
-  <circle
-    fill='none'
-    stroke='#000'
-    strokeWidth='0.5'
-    cx='64'
-    cy='32'
-  />
-</g>
-)
-
-const text = (
-  <g>
-    <g fill='none' stroke='#0ff' strokeWidth='0.125'>
-      <rect x='40' y='12' width='48' height='2' />
-      <rect x='36' y='16' width='56' height='2' />
-      <rect x='34' y='20' width='60' height='2' />
-      <rect
-        x='32'
-        y='24'
-        width='64'
-        height='16'
-      />
-      <rect x='34' y='42' width='60' height='2' />
-      <rect x='36' y='46' width='56' height='2' />
-      <rect x='40' y='50' width='48' height='2' />
-    </g>
-    <text
-      x='64'
-      y='36.5'
-      fill='#0f0'
-      textAnchor='middle'
-    >
-      MDXS
-    </text>
-  </g>
-)
-
-const wires = (
-  <g
-    transform='translate(48 16)'
-    fill='none'
-    stroke='#0f0'
-    strokeWidth='.125'>
-    <path
-      d={[
-        'M 0 0',
-        'L 32 32',
-        'L 0 32',
-        'z',
-        'M 32 0',
-        'L 0 32',
-        'L 32 32',
-        'z'
-      ].join(' ')}
+export default ({
+  width = 1024,
+  color = '#0d0',
+  bg = '#000'
+}) =>
+  <svg
+    viewBox='0 0 256 128'
+    width={width}
+    height={width / 2}
+    style={{
+      maxWidth: '100%'
+    }}>
+    <rect
+      fill={bg}
+      width='256'
+      height='128'
     />
-  </g>
-)
+    <g transform='translate(80 16)'>
+      <Logo
+        bg={color}
+        size={96}
+      />
+    </g>
+  </svg>
