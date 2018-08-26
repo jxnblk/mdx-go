460c61528e961eff4dff17e2429bf99059c1ac88
Brent Jackson <jxnblk@gmail.com>
2018-08-26 14:43:32 -0400

    Use app-module-path

---
+++ examples/styled-components/README.md
@@ -0,0 +1,10 @@
+
+# styled-components example
+
+```sh
+npm i -g mdx-go && npm i
+```
+
+```sh
+npm start
+```
---
+++ examples/styled-components/docs/Box.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  padding: 32px;
+  font-size: 32px;
+  font-weight: bold;
+  background-color: tomato;
+`
+
+export default Box
