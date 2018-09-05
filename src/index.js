30bae5a0918d7b55bb9c62c7f4aeee4cd1e4266a
Brent Jackson <jxnblk@gmail.com>
2018-09-05 16:17:13 -0400

    Disable emotion components

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ export { default as Link } from './Link'
 export { ScrollTop } from './ScrollTop'
 
 // unstyled MDXProvider with LiveCode and Link
-export { ComponentProvider } from './ComponentProvider'
-export { withComponents } from './withComponents'
-
-export {
-  Layout,
-  Sidebar,
-  Main,
-  MenuToggle,
-  MenuIcon,
-  withLayout,
-} from './Layout'
-export {
-  NavLinks,
-  NavLink
-} from './NavLinks'
-export { Pagination } from './Pagination'
-export { LiveCode } from './LiveCode'
-
-// styles for MDX elements
-export { StyleProvider } from './StyleProvider'
-
-// zero-config layout
-export { DocsLayout } from './DocsLayout'
-export { DevLayout } from './DevLayout'
+// export { ComponentProvider } from './ComponentProvider'
+// export { withComponents } from './withComponents'
+//
+// export {
+//   Layout,
+//   Sidebar,
+//   Main,
+//   MenuToggle,
+//   MenuIcon,
+//   withLayout,
+// } from './Layout'
+// export {
+//   NavLinks,
+//   NavLink
+// } from './NavLinks'
+// export { Pagination } from './Pagination'
+// export { LiveCode } from './LiveCode'
+//
+// // styles for MDX elements
+// export { StyleProvider } from './StyleProvider'
+//
+// // zero-config layout
+// export { DocsLayout } from './DocsLayout'
+// export { DevLayout } from './DevLayout'
